b68b42f611118a41d16740ca8b8097417313d383
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-08-06 16:30:26 -0300

    finalizando comentarios no meu codigo

--- src/Components/Robot.tsx/robot.tsx
+++ src/Components/Robot.tsx/robot.tsx
@@ -1,15 +1,28 @@
+//aqui nas primeiras linhas do meu código estou importando todos
+//os exports do meu arquivo roboStyle, importei também a imagem do
+//meu robo, e por último importei meu componente Recompensa
 import * as S from "./robotStyle.tsx"
 import Robo from "../Robot.tsx/robo.png"
 import Recompensa from "./Recompensa.tsx/recompensa.tsx";
 
+//aqui abaixo eu defini um tipo TypeScript RobotProps que está
+//especifícando que o meu component robot deve receber três props
+//e que essas props são todas do tipo 'string'
 type RobotProps = {
     tipo: string;
     cargo: string;
     identificador: string;
 };
 
+//criei o meu componente funcional robot que aceita tipo, 
+//cargo e identificador como props
 function Robot({ tipo, cargo, identificador }: RobotProps) {
 
+//estou retornando o meu componente funcional, dentro do meu
+//fragmento <> coloquei alguns elementos, meu h1 exibe o tipo
+//do meu robo, o meu h3 vai exibir o cargo do meu robo e o meu
+//h4 vai exibir o identificador do meu robo, minha Div4 contém
+//o meu componente Recompensa e está repassando o valorRecompensa como 10
     return (
         <>
             <S.Div>
@@ -33,4 +46,7 @@ function Robot({ tipo, cargo, identificador }: RobotProps) {
     )
 }
 
+//aqui abaixo estou exportando meu componente Robot
+//e estou permitindo que ele seja usado em outros 
+//arquivos caso eu quiser
 export default Robot
