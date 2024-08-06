e297dd7fef3a19d320e03915df9a0645522d1de7
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-08-05 22:30:20 -0300

    criação do component robot

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import Robot from "./Components/Robot.tsx/robot"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+*{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+`
+
 function App() {
 
   return (
     <>
+    <GlobalStyle/>
+    <Robot/>
     </>
   )
 }
---
+++ src/Components/Robot.tsx/robot.tsx
@@ -0,0 +1,11 @@
+import * as S from "./robotStyle.tsx"
+
+function Robot() {
+    return(
+        <S.Div>
+          
+        </S.Div>
+    )
+}
+
+export default Robot
